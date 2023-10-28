6fadac33ced7413054ca4f51f144721fe4c9c490
utya1414 <utya1414@gmail.com>
2023-10-28 11:20:46 +0900

    apply tailwind correctoly and import tailwind-variant

--- src/components/SidebarIcon.tsx
+++ src/components/SidebarIcon.tsx
@@ -2,8 +2,8 @@ import Icon from "../images/icon.png";
 
 const SidebarIcon = () => {
   return (
-    <div className="pt-5 pb-4 text-center">
-        <img src={Icon} alt="" className="w-2/3 hover:cursor-pointer" />
+    <div className="pt-5 pb-4 text-center" >
+        <img src={Icon} alt="" className="w-1/2 block m-auto" />
         <p className="text-white text-xl font-medium">utya1414@gmail.com</p>
     </div>
   );
